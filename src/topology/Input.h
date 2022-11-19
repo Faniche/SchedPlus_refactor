1fd3e9e25622e8963f0b7e760468e4681f5d9755
faniche <guoyanan23215@gmail.com>
2022-11-19 23:10:22 +0800

    change "flow" to "stream" and modify impl of vSetFlows from random generation to reading from json file

--- src/topology/Input.h
+++ src/topology/Input.h
@@ -11,22 +11,24 @@
 #include "../components/Node.h"
 #include "../components/DirectedLink.h"
 #include "../components/Stream.h"
+#include "../../lib/json/json.hpp"
 
 class Input {
 private:
     virtual void vSetNodesAndLinks() = 0;
-    virtual void vSetFlows(uint32_t flowNum) = 0;
+    virtual void vSetStreams(uint32_t streamsNum, std::string streamFilePath) = 0;
 public:
     std::vector<std::shared_ptr<Node>> nodes;
     std::vector<std::shared_ptr<Node>> esList;
     std::vector<std::shared_ptr<Node>> swList;
     std::vector<std::shared_ptr<DirectedLink>> links;
-    std::vector<std::shared_ptr<Stream>> flows;
-    std::map<node_id, std::shared_ptr<Node>> nodeMap;
+    std::vector<std::shared_ptr<Stream>> streams;
+    std::map<node_id, std::shared_ptr<Node>> nodeIdMap;
+    std::map<std::string, std::shared_ptr<Node>> nodeNameMap;
 
     void setNodesAndLinks();
 
-    void setFlows(uint32_t flowNum);
+    void setStreams(uint32_t streamsNum, std::string streamFilePath);
 };
 
 
