08d29608e5386a6462d85f55e287ae1c234a2ae8
faniche <guoyanan23215@gmail.com>
2022-11-19 23:08:23 +0800

    change spdlog to local code and redefine PRIORITY_CODE_POINT

--- src/type.h
+++ src/type.h
@@ -8,21 +8,30 @@
 #include <cstdint>
 #include <memory>
 #include <iostream>
-#include <spdlog/spdlog.h>
+#include "../lib/spdlog/include/spdlog/spdlog.h"
 
 /**
  * @brief Definition of Priority Code Point for vlan frame https://en.wikipedia.org/wiki/IEEE_P802.1p
  */
-enum PRIORITY_CODE_POINT {
-    P0,     // 0 (lowest)       Background
-    P1,     // 1 (default)      Best effort
-    P2,     // 2                Excellent effort
-    P3,     // 3                Critical applications
-    P4,     // 4                Video, < 100 ms latency and jitter
-    P5,     // 5                Voice, < 10 ms latency and jitter
-    P6,     // 6                Internetwork control
-    P7      // 7 (highest)      Network control
-};
+typedef uint8_t PRIORITY_CODE_POINT;
+constexpr PRIORITY_CODE_POINT P0 = 0;     // 0 (lowest)       Background
+constexpr PRIORITY_CODE_POINT P1 = 1;     // 1 (default)      Best effort
+constexpr PRIORITY_CODE_POINT P2 = 2;     // 2                Excellent effort
+constexpr PRIORITY_CODE_POINT P3 = 3;     // 3                Critical applications
+constexpr PRIORITY_CODE_POINT P4 = 4;     // 4                Video, < 100 ms latency and jitter
+constexpr PRIORITY_CODE_POINT P5 = 5;     // 5                Voice, < 10 ms latency and jitter
+constexpr PRIORITY_CODE_POINT P6 = 6;     // 6                Internetwork control
+constexpr PRIORITY_CODE_POINT P7 = 7;     // 7 (highest)      Network control
+//enum PRIORITY_CODE_POINT {
+//    P0,     // 0 (lowest)       Background
+//    P1,     // 1 (default)      Best effort
+//    P2,     // 2                Excellent effort
+//    P3,     // 3                Critical applications
+//    P4,     // 4                Video, < 100 ms latency and jitter
+//    P5,     // 5                Voice, < 10 ms latency and jitter
+//    P6,     // 6                Internetwork control
+//    P7      // 7 (highest)      Network control
+//};
 
 /**
  * @brief Inter Frame Gap, in Gigbit ethernet: 96ns
