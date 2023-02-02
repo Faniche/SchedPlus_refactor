d2524eab61fbfa104a27edf9e90e3800722d5f1a
Faniche <guoyanan23215@gmail.com>
2023-02-03 01:30:08 +0800

    add strategy parameter for upload

--- upload_solution.sh
+++ upload_solution.sh
@@ -4,12 +4,13 @@ solution_dir="cmake-build-debug/SolutionReport"
 
 # $1: topology
 # $2: streamNum
-# $3: runId
-# $4: solutionId
-upload_xmls=$(ls $solution_dir/"$1"/"$2"/"$3"/"$4"_*.xml)
+# $3: strategy
+# $4: runId
+# $5: solutionId
+upload_xmls=$(ls $solution_dir/"$1"/"$2"/"$3"/"$4"/"$5"_*.xml)
 for file in $upload_xmls
   do
     docker cp $file nesting:/root/models/nesting/simulations/schedplus/xml/"$1"/
   done
-ini_file=$(ls $solution_dir/"$1"/"$2"/"$3"/"$4"_*.ini)
+ini_file=$(ls $solution_dir/"$1"/"$2"/"$3"/"$4"/"$5"_*.ini)
 docker cp $ini_file nesting:/root/models/nesting/simulations/schedplus/
